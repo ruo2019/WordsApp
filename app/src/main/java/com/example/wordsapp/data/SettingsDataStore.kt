8d111f48e3e235ba8d9518465d55a398efd86a3f
ruo2019 <ruo2019@outlook.com>
2022-09-21 22:27:07 -0700

    Key type functions

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -3,11 +3,14 @@ package com.example.wordsapp.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = LAYOUT_PREFERENCES_NAME)
+
+private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
 
 class SettingsDataStore(context: Context) {
 
